f0277b05dcf229f102441112a72fe5186894c7a4
Vladimir Koniakhin <vakonyakhin@gmail.com>
2025-11-07 10:07:53 +0300

    Implement jaundice rate calculation pipeline

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import aiohttp
 import asyncio
 
+import pymorphy2
+
 from adapters.inosmi_ru import sanitize
+from text_tools import split_by_words, calculate_jaundice_rate
 
 
 async def fetch(session, url):
@@ -11,11 +14,16 @@ async def fetch(session, url):
 
 
 async def main():
+    morph = pymorphy2.MorphAnalyzer()
+
     url = 'https://inosmi.ru/20251106/evropa-275505851.html'
     async with aiohttp.ClientSession() as session:
         html = await fetch(session, url)
-        text = sanitize(html, plaintext=False)
-        print(text)
+        text = sanitize(html, plaintext=True)
+        words = split_by_words(morph, text)
+        charged_words = ['есть', 'полный', 'право','россия','запад', 'это']
+        rate = calculate_jaundice_rate(words, charged_words)
+        print(rate)
 
 
 asyncio.run(main())
