f42dcff8cac55109774ac42072470e383b23c823
Vladimir Koniakhin <vakonyakhin@gmail.com>
2025-11-20 16:45:32 +0300

    implement timeout and test for split_by_words

--- text_tools.py
+++ text_tools.py
@@ -1,5 +1,12 @@
 import pymorphy2
 import string
+import asyncio
+from async_timeout import timeout
+
+import pytest
+
+
+pytest_plugins = ('pytest_asyncio')
 
 
 def _clean_word(word):
@@ -9,25 +16,51 @@ def _clean_word(word):
     return word
 
 
-def split_by_words(morph, text):
+async def split_by_words(morph, text):
     """Учитывает знаки пунктуации, регистр и словоформы, выкидывает предлоги."""
+    # The timeout is now controlled by the caller.
     words = []
-    for word in text.split():
-        cleaned_word = _clean_word(word)
-        normalized_word = morph.parse(cleaned_word)[0].normal_form
-        if len(normalized_word) > 2 or normalized_word == 'не':
-            words.append(normalized_word)
+    try:
+        async with timeout(3):
+            for word in text.split():
+                cleaned_word = _clean_word(word)
+                normalized_word = morph.parse(cleaned_word)[0].normal_form
+                if len(normalized_word) > 2 or normalized_word == 'не':
+                    words.append(normalized_word)
+                await asyncio.sleep(0)
+    except asyncio.exceptions.TimeoutError:
+        words = []
+        raise asyncio.exceptions.TimeoutError
+
     return words
 
 
-def test_split_by_words():
-    # Экземпляры MorphAnalyzer занимают 10-15Мб RAM т.к. загружают в память много данных
-    # Старайтесь организовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
-    morph = pymorphy2.MorphAnalyzer()
+@pytest.fixture(scope="module")
+# Экземпляры MorphAnalyzer занимают 10-15Мб RAM т.к. загружают в память много данных
+# Старайтесь организовать свой код так, чтоб создавать экземпляр MorphAnalyzer заранее и в единственном числе
+def morph():
+    return pymorphy2.MorphAnalyzer()
 
-    assert split_by_words(morph, 'Во-первых, он хочет, чтобы') == ['во-первых', 'хотеть', 'чтобы']
+@pytest.fixture(scope="module")
+def text():
+    with open('test.txt', 'r', encoding='utf-8') as file:
+        text = file.read()
+    return text
 
-    assert split_by_words(morph, '«Удивительно, но это стало началом!»') == ['удивительно', 'это', 'стать', 'начало']
+@pytest.mark.asyncio
+async def test_split_by_words(morph):
+
+    assert await split_by_words(morph, 'Во-первых, он хочет, чтобы') == ['во-первых', 'хотеть', 'чтобы']
+
+    assert await split_by_words(morph, '«Удивительно, но это стало началом!»') == ['удивительно', 'это', 'стать', 'начало']
+
+@pytest.mark.asyncio
+async def test_split_by_words_timeout(morph, text):
+    words = []
+    with pytest.raises(asyncio.TimeoutError):
+        await split_by_words(morph, text)
+
+    assert words == []
 
 
 def calculate_jaundice_rate(article_words, charged_words):
